eb8f269e0b93304bd96d3a1b1dd5bde64c6e7cb2
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-07-14 17:02:51 +0100

    Web application setup

---
+++ lib/database_connection.rb
@@ -0,0 +1,29 @@
+# file: lib/database_connection.rb
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'pg'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
+
+
+class DatabaseConnection
+  # This method connects to PostgreSQL using the 
+  # PG gem. We connect to 127.0.0.1, and select
+  # the database name given in argument.
+  def self.connect(database_name)
+    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+  end
+
+  # This method executes an SQL query 
+  # on the database, providing some optional parameters
+  # (you will learn a bit later about when to provide these parameters).
+  def self.exec_params(query, params)
+    @connection.exec_params(query, params)
+  end
+end
